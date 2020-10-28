0cbc0fb4cd2c2ab9f02b0a9981f247b5249d220d
Manish Mahant <manishmahant>
2020-10-28 11:01:01 +0530

    ajax and HTTP request

--- js/Http Request & Ajax/app.js
+++ js/Http Request & Ajax/app.js
@@ -4,15 +4,29 @@ function fetchPosts () {
     var url = 'https://jsonplaceholder.typicode.com/todos';
     xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-      console.log('onreadystatechange', xhr.readyState)
-      if(xhr.readyState === 4) {
-        var response = JSON.parse(xhr.response)
-        console.log(typeof xhr.response);
-        console.log(typeof response);
-      }
+        console.log('onreadystatechange', xhr.readyState)
+        if(xhr.readyState === 4) {
+            var response = JSON.parse(xhr.response)
+            console.log(typeof xhr.response);
+            console.log(typeof response);
+        }
     }
     xhr.open('GET',url);
     xhr.send();
-    console.dir(xhr);
 }
 
+function getPostsWithAjax () {
+    var url = 'https://jsonplaceholder.typicode.com/todos';
+    $.ajax({
+        type: 'GET',
+        url,
+        dataType:'json',
+        data:{hello:'hello'},
+        success: function (resp) {
+            console.log(resp[0]);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
