02389fd5573a08efb710c87ccc8659275bb69cfa
Manish Mahant <manishmahant>
2020-10-30 11:24:59 +0530

    package update

--- js/Package Manager/index.js
+++ js/Package Manager/index.js
@@ -6,13 +6,15 @@ var movies = [ 'm1', 'm2', 'm3'];
 // var movies = null;
 
 console.log('Hello NPM 1');
-lodash.forEach(movies, function(movie){
-    console.log(movie)
-});
+
+function foreachCallback(item, index) {
+    console.log(item, index);
+}
+
+lodash.forEach(movies, foreachCallback);
+
 console.log('Hello NPM 2');
 if(movies !== null) {
-    movies.forEach(function(m,i){
-        console.log(i,m)
-    });
+    movies.forEach(foreachCallback);
 }
 console.log('Hello NPM 3');
