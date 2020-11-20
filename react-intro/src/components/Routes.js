34e361203a27c48fa351e849f6fee722523d2346
Manish Mahant <manishmahant>
2020-11-20 11:08:06 +0530

    404 handling

--- react-intro/src/components/Routes.js
+++ react-intro/src/components/Routes.js
@@ -1,12 +1,22 @@
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch, Link } from "react-router-dom";
 import Home from "../pages/Home";
 import Information from "../pages/Information";
+
+const FourZeroFour = (props) => {
+    console.log(props)
+    props.history.push('/');
+    return (
+        <Link to={'/'}><div>No Page Found</div> Go Back to home</Link>
+    );
+};
 
 const Routes = () => {
     return (
         <Switch>
             <Route path="/info" exact component={Information} />
             <Route path="/" exact component={Home} />
+            <Route path="*" component={FourZeroFour} />
         </Switch>
     );
 };
