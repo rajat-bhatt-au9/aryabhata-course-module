aa57e5a8067d9f84f02f77e7f3836a4a6dae7e57
aakashhanda <akashhanda@attainu.com>
2020-12-10 13:52:41 +0530

    a

---
+++ hotel_app_react/src/Component/Home/Search.js
@@ -0,0 +1,88 @@
+import React,{Component} from 'react';
+import './Search.css';
+
+const url ="https://developerfunnel.herokuapp.com/location";
+const hotelurl ="https://developerfunnel.herokuapp.com/hotels?city=";
+
+class Search extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            location:'',
+            hotels:''
+        }
+    }
+
+    renderCity = (data) => {
+        if(data){
+            return data.map((item) =>{
+                return(
+                    <option value={item.city}>
+                        {item.city_name}
+                    </option>
+                )
+            })
+        }
+    }
+
+    handleChangeCity=(e) => {
+        console.log(e.target.value)
+        const cityid = e.target.value
+        fetch(`${hotelurl}${cityid}`,{method:'GET'})
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({hotels:data})
+        })
+    }
+
+    renderHotel = (data) => {
+        if(data){
+            return data.map((item) =>{
+                return(
+                    <option value={item._id}>
+                        {item.name} | {item.locality}
+                    </option>
+                )
+            })
+        }
+    }
+
+    render(){
+        console.log(">>>>",this.state.location)
+        return(
+            <div className="imageContainer">
+                <div id="logo">
+                    D!
+                </div>
+                <div className="heading">
+                    Plan Trip With Us
+                </div>
+                <div className="locationSelector">
+                    <select className="locationDropDown" onChange={this.handleChangeCity}>
+                        <option>-----SELECT Location------</option>
+                         {this.renderCity(this.state.location)}
+                    </select>
+                    <select className="reataurantsinput">
+                        <option>-----SELECT Hotel------</option>
+                        {this.renderHotel(this.state.hotels)}
+                    </select>
+                </div>
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        fetch(url,{method:'GET'})
+        .then((res) => res.json()) //promise of api
+        // we will get the data
+        .then((data) => {
+            this.setState({location:data})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+}
+
+export default Search;
